65486f714ff33ae006288086eb0eae2619ce46af
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-08 18:32:56 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "1656567556-8kxZVdJv";
+    var liffId = "1656567556-GW67e4wB";
     initializeLiff(liffId);
 })
 
