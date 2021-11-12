1719afa6d1db543cdee1d3df4f4595979be827a1
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-12 17:05:29 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -91,9 +91,9 @@ function closeWin() {
 
 //バリデーションテスト
 function check(){
-    if (googleform1.mail.value == ""){
+    if (googleform1.username.value == ""){
         //条件に一致する場合(メールアドレスが空の場合)
-        alert("メールアドレスを入力してください");    //エラーメッセージを出力
+        alert("お名前を入力してください");    //エラーメッセージを出力
         return false;    //送信ボタン本来の動作をキャンセルします
     }else{
         //条件に一致しない場合(メールアドレスが入力されている場合)
