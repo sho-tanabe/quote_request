d93569831073469d061aaae7a1019081a0492f6f
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-14 04:37:54 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -90,16 +90,16 @@ function closeWin() {
 
 
 //バリデーションテスト
-function check(){
-    if (googleform1.username.value == ""){
+//function check(){
+//    if (googleform1.username.value == ""){
         //条件に一致する場合(メールアドレスが空の場合)
-        alert("お名前を入力してください");    //エラーメッセージを出力
-        return false;    //送信ボタン本来の動作をキャンセルします
-    }else{
+//        alert("お名前を入力してください");    //エラーメッセージを出力
+//        return false;    //送信ボタン本来の動作をキャンセルします
+//    }else{
         //条件に一致しない場合(メールアドレスが入力されている場合)
-        return true;    //送信ボタン本来の動作を実行します
-    }
-}
+//        return true;    //送信ボタン本来の動作を実行します
+//    }
+//}
 //バリデーションテスト
 
 
